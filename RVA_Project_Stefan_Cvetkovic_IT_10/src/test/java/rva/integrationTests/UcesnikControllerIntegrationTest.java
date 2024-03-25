821c24224fe5b5a2ce0d6d738dcde2fa20632636
it10-2021 <it10-2021@AR2-10.ftn.ns>
2024-03-25 15:30:15 +0100

    Vezbe 5 - Testiranje SudControllera i UcesnikControllera

---
+++ RVA_Project_Stefan_Cvetkovic_IT_10/src/test/java/rva/integrationTests/UcesnikControllerIntegrationTest.java
@@ -0,0 +1,156 @@
+package rva.integrationTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+
+import rva.models.Sud;
+import rva.models.Ucesnik;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class UcesnikControllerIntegrationTest {
+
+	@Autowired
+	TestRestTemplate template;
+	
+	int highestId;
+	
+	void createHighestId() {
+		ResponseEntity<List<Ucesnik>> response = template
+				.exchange("/ucesnik", HttpMethod.GET,
+						null,
+						new ParameterizedTypeReference<List<Ucesnik>>() {} );
+		ArrayList<Ucesnik> list = (ArrayList<Ucesnik>) response.getBody();
+		for(int i = 0; i < list.size(); i++) {
+			if(highestId <= list.get(i).getId()) {
+				highestId = list.get(i).getId() + 1;
+			}
+		}
+		
+	}
+	
+
+	void getHighestId() {
+		createHighestId();
+		highestId--;
+	}
+	
+
+	@Test
+	@Order(1)
+	void testGetAllUcesnik() {
+		ResponseEntity<List<Ucesnik>> response = template.exchange("/ucesnik", HttpMethod.GET, 
+				null, new ParameterizedTypeReference<List<Ucesnik>>() {});
+		
+		int statusCode = response.getStatusCode().value();
+		List<Ucesnik> ucesnici = response.getBody();
+		
+		assertEquals(200, statusCode);
+		assertTrue(!ucesnici.isEmpty());
+		
+		
+	}
+	
+	@Test
+	@Order(2)
+	void testUcesnikById() {
+		int id = 1;
+		ResponseEntity<Ucesnik> response = template.exchange("/ucesnik/id/" + id, HttpMethod.GET, null, Ucesnik.class);
+		
+		int statusCode = response.getStatusCode().value();
+		
+		assertEquals(200, statusCode);
+		assertNotNull(response.getBody());
+		assertEquals(id, response.getBody().getId());
+	}
+	
+	@Test
+	@Order(3)
+	void testGetUcesnikByMbr() {
+		String mbr = "4";
+		ResponseEntity<List<Ucesnik>> response = template.exchange("/ucesnik/mbr/" + mbr, HttpMethod.GET, 
+				null, new ParameterizedTypeReference<List<Ucesnik>>() {} );
+		int statusCode = response.getStatusCode().value();
+		List<Ucesnik> ucesnici = response.getBody();
+		
+		assertEquals(200, statusCode);
+		assertNotNull(response.getBody());
+		//assertEquals(naziv, response.getBody().ge);
+		for(Ucesnik u: ucesnici) {
+			assertTrue(u.getMbr().contains(mbr));
+		
+		}
+	}
+	
+
+	
+	@Test
+	@Order(4)
+	void testCreateUcesnik() {
+		Ucesnik ucesnik = new Ucesnik();
+		ucesnik.setIme("POST ime");
+		ucesnik.setMbr("POST mbr");
+		
+		HttpEntity<Ucesnik> entity = new HttpEntity<Ucesnik>(ucesnik);
+		createHighestId();
+		ResponseEntity<Ucesnik> response = template
+				.exchange("/ucesnik", HttpMethod.POST,
+						entity, Ucesnik.class);
+		int statusCode = response.getStatusCode().value();
+		
+		assertEquals(201, statusCode);
+		assertEquals("/ucesnik/id/" + highestId, 
+				response.getHeaders().getLocation().getPath());
+		assertEquals(ucesnik.getIme(), response.getBody().getIme());
+		assertEquals(ucesnik.getMbr(), response.getBody().getMbr());
+	}
+	
+	@Test
+	@Order(5)
+	void testUpdateUcesnik() {
+		Ucesnik ucesnik = new Ucesnik();
+		ucesnik.setIme("POST ime");
+		ucesnik.setMbr("POST mbr");;
+		
+		HttpEntity<Ucesnik> entity = new HttpEntity<Ucesnik>(ucesnik);
+		getHighestId();
+		ResponseEntity<Ucesnik> response = template
+				.exchange("/ucesnik/id/" + highestId, HttpMethod.PUT,
+						entity, Ucesnik.class);
+		int statusCode = response.getStatusCode().value();
+		
+		assertEquals(200, statusCode);
+		assertTrue(response.getBody() instanceof Ucesnik);
+		assertEquals(ucesnik.getIme(), response.getBody().getIme());
+		assertEquals(ucesnik.getMbr(), response.getBody().getMbr());
+	}
+	
+	@Test
+	@Order(6)
+	void testDeleteUcesnikById() {
+		getHighestId();
+		ResponseEntity<String> response = template
+				.exchange("/ucesnik/id/" + highestId, HttpMethod.DELETE,
+						null, String.class);
+	
+		
+		assertEquals(200, response.getStatusCode().value());
+		assertTrue(response.getBody().contains("has been deleted"));
+
+	}
+	
+}
